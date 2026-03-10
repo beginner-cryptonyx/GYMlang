00f1b2a9c201e15b03378850b5b1db0bd749171d
beginner-cryptonyx <mayanksethi242008@gmail.com>
2026-03-10 09:21:01 +0530

    impliment test results into transcriber

--- GL.py
+++ GL.py
@@ -1,7 +1,16 @@
 import sys
 import os
-import re
+import tokenize
+import io
 
+keywords = {
+    'natty':'def',
+    'failure': 'return',
+    'jacked':'if',
+    'rookie': 'else',
+    'jucied': 'elif',
+    'skip_legs': 'pass'
+}
 # check if there is **a** file that is passed into the gl.py
 number_of_argments = len(sys.argv)
 if number_of_argments != 2:
@@ -13,10 +22,37 @@ if not os.path.isfile(sys.argv[1]):
 
 # print the content of the file
 with open(sys.argv[1], 'r') as f:
-    target_lines = []
-    for i, line in enumerate(f.readlines()):
-        if re.search(f"\\bnatty\\b", line) and not re.search("(\'|\").*natty.*(\"|\')", line) and not re.search("#.*natty", line):
-            target_lines.append(i)
-    print(target_lines)
+    source = f.read()
+    tokens = list(tokenize.generate_tokens(io.StringIO(source).readline))
 
+    for token in tokens:
+        print(token)
 
+    new_tokens = []
+    for token in tokens:
+        if token.type not in [59,60,61,62,63,64,65,3]:
+            # now sub out all the keywords
+            string = token.string
+            for old, new in keywords.items():
+                string.replace(old,new)
+            new_token = tokenize.TokenInfo(
+                type = token.type,
+                string = string,
+                start = token.start,
+                end = token.end,
+                line = token.line.replace('natty', 'def').replace('rack', '=')
+            )
+            new_tokens.append(new_token)
+        else:
+            new_tokens.append(token)
+    print('/-------/')
+    print('/-------/')
+    print('/-------/')
+    print('/-------/')
+    print('/-------/')
+    
+    for token in new_tokens:
+        print(token)
+    
+    new_code = tokenize.untokenize(new_tokens)
+    print(new_code)
